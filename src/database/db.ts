042a63003c56847a730b3be360cdabf45021bb89
Alex Medina <afmedina@uniguajira.edu.co>
2023-10-09 19:19:22 -0500

    Conexion con sqlserver

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,30 +1,30 @@
 const Sequelize = require('sequelize');
 
-const DB_NAME = 'seguros_mysql_nodejs';
+const DB_NAME = 'seguros_sqlserver_nodejs';
 
-const DB_USER = 'root';
+const DB_USER = 'sa';
 
 const DB_PASS = '12345678';
 
 
 
 export const database = new Sequelize(
-    DB_NAME,
-    DB_USER,
-    DB_PASS,
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
 
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    }
+  {
+      host: 'localhost',
+      dialect: 'mssql',
+      port: 1433
+  }
 
 );
 
 
 async function generateDb() {
-    await database.sync({ force: false })
-    console.log('Base de datos y tablas creada');
+  await database.sync({ force: false })
+  console.log('Base de datos y tablas creada');
 }
 
 generateDb();
